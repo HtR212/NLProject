0ca950d5363eb6948415827d7530cebe5e51bbf8
hanzhi713 <hanzhi713@163.com>
2021-12-10 20:21:16 -0500

    cleanup and add top2 eval

--- eval.py
+++ eval.py
@@ -22,8 +22,10 @@ for fname in tqdm(os.listdir(test_dir), desc="[Inferencing]"):
 
     with torch.no_grad():
         predictions = model(article_tokens.cuda(), option_ids)
-        preds = torch.argmax(predictions, -1)
-        corrects += (preds == answers.cuda()).sum()
-        total += len(answers)
+        preds = torch.topk(predictions, 2, -1).indices
 
+        temp = preds[:, 0] == answers.cuda()
+        corrects += temp.sum()
+        corrects_top2 += (temp | (preds[:, 1] == answers.cuda())).sum()
+        total += len(answers)
         tqdm.write(f"{corrects}/{total} = {corrects/total*100}%, {corrects_top2}/{total} = {corrects_top2/total*100}%")
